d554541ffb7ac1a2d2e3dc49c9d6aeab31a28579
Alexya Moraes <alexyavianaa@gmail.com>
2025-03-20 16:56:17 -0400

    ajustes no process

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://copia-spotify-full-stack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
